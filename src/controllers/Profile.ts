b3b936b8b1f73aac95d0ddb3507b34b3dd4a757f
José Ovando <42974480+Jos260400@users.noreply.github.com>
2021-09-04 20:51:52 -0600

    Update Profile.ts

--- src/controllers/Profile.ts
+++ src/controllers/Profile.ts
@@ -2,4 +2,4 @@ import { Request, Response } from 'express';
 
 export const Fernando = (req: Request, res: Response): void => { res.json({ response: '18071' }); };
 
-export const Fernando = (req: Request, res: Response): void => { res.json({ response: 'Computación' }); };
+

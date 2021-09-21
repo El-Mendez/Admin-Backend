6d1c687e1489bb956cb3db49d946cdbdd271fb12
Méndez <pablomendez778@gmail.com>
2021-09-20 18:48:36 -0600

    instalando dependencias para unit test

--- src/controllers/Profile.ts
+++ src/controllers/Profile.ts
@@ -1,33 +1,33 @@
 import { Request, Response } from 'express';
 import { connection } from '../services/connection';
-import * as Schema from "../validators/Authorization";
+import * as Schema from '../validators/Authorization';
 
 export const personalProfile = (
-    req: Request,
-    res: Response,
+  req: Request,
+  res: Response,
 ): void => {
-    connection
-        .query(`
+  connection
+    .query(`
             select * 
             from profile 
             where carne = $1;`, [req.carne])
-        .then((response) => {
-            if (!response.rows[0]) { res.status(403).json({ err: 'User without courses or hobbies.' }); return; }
+    .then((response) => {
+      if (!response.rows[0]) { res.status(403).json({ err: 'User without courses or hobbies.' }); return; }
 
-            res.json(response.rows); });
+      res.json(response.rows);
+    });
 };
 
 export const userProfile = (req: Request<{}, {}, Schema.profileSchema>, res: Response): void => {
-    const userId: number = req.body.carne;
-    connection
-        .query(`
+  const userId: number = req.body.carne;
+  connection
+    .query(`
             select * 
             from profile 
             where carne = $1;`, [userId])
-        .then((response) => {
-            if (!response.rows[0]) { res.status(403).json({ err: 'User without courses or hobbies.' }); return; }
+    .then((response) => {
+      if (!response.rows[0]) { res.status(403).json({ err: 'User without courses or hobbies.' }); return; }
 
-            res.json(response.rows); });
+      res.json(response.rows);
+    });
 };
-
-

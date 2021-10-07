6523d2d33bae126f1bd1ef8b32fe7e3a71d8e487
Zaray <cor191025@uvg.edu.gt>
2021-10-06 23:47:46 -0600

    agregando redes sociales a la información del perfil

--- setup/views.sql
+++ setup/views.sql
@@ -2,7 +2,8 @@
 CREATE OR REPLACE VIEW profile AS
 SELECT u.carne, CONCAT(u.nombre, ' ', u.apellido) AS nombre_completo, c.nombre AS carrera, u.correo,
        array_agg(DISTINCT (c2.nombre || ' → sección: ' || s.seccion)) cursos,
-       array_agg(DISTINCT h.nombre) hobbies
+       array_agg(DISTINCT h.nombre) hobbies,
+       array_agg(DISTINCT (ps.nombre || ': ' || rs.perfil)) redes_sociales
 FROM usuario u
          INNER JOIN carrera c on c.id = u.carrera_id
          INNER JOIN asiste_seccion "as" on u.carne = "as".usuario_carne
@@ -10,6 +11,8 @@ FROM usuario u
          INNER JOIN curso c2 on c2.id = s.curso_id
          INNER JOIN has_hobby hh on u.carne = hh.usuario_carne
          INNER JOIN hobby h on h.id = hh.hobby_id
+         INNER JOIN red_social rs on u.carne = rs.usuario_carne
+         INNER JOIN plataforma_social ps on rs.plataforma_id = ps.id
 GROUP BY u.carne, CONCAT(u.nombre, '', u.apellido), c.nombre, u.correo;
 
 -- Vista para extraer los datos del usuario para mostrar en su perfile

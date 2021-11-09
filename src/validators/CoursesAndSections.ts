1b8213edd610779f232b79b908616d27e0fc22cc
Méndez <47764656+El-Mendez@users.noreply.github.com>
2021-11-08 21:03:42 -0600

    Cree la ruta de eliminar secciones y agregué pruebas

--- src/validators/CoursesAndSections.ts
+++ src/validators/CoursesAndSections.ts
@@ -7,3 +7,10 @@ export const assignSection = [
   body('seccionesId').isArray({ min: 1 }),
   body('seccionesId.*').isInt({ min: -1 }).toInt(10),
 ];
+
+export interface DeleteSectionSchema {
+  seccionId: number
+}
+export const deleteSection = [
+  body('seccionId').isInt({ min: -1 }).toInt(10),
+];
